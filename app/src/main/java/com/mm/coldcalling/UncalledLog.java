1b620d24eff66735a92c842c3b654f7585cce50c
Michael Wu <62720843+michaelwudev@users.noreply.github.com>
2021-10-12 22:55:11 -0400

    Fixed image names and other activities

--- app/src/main/java/com/mm/coldcalling/UncalledLog.java
+++ app/src/main/java/com/mm/coldcalling/UncalledLog.java
@@ -2,32 +2,25 @@ package com.mm.coldcalling;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.WindowManager;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.ListView;
+
 
 
 public class UncalledLog extends AppCompatActivity {
+
+  private ListView studentsListView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_uncalled_log);
 
-    //set up uncalled log
-    LinearLayout uncalledList = findViewById(R.id.uncalledList);
+    this.studentsListView = findViewById(R.id.student_list);
 
-    //adds textviews to the linearlayout
-    for(int i = 0; i < MainActivity.uncalledStudents.size(); i++){
-
-      TextView tv = new TextView(this);
-      tv.setText(MainActivity.uncalledStudents.get(i).getFullName());
-      tv.setId(i);
-      WindowManager.LayoutParams textViewLayoutParams = new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
-      tv.setLayoutParams(textViewLayoutParams);
-      uncalledList.addView(tv);
-
-    }
+    // create adapter
+    CustomAdapater customAdapater = new CustomAdapater(this, MainActivity.uncalledStudents);
+    // set adapter
+    this.studentsListView.setAdapter(customAdapater);
 
   }
 }
